cad6ae6f7ceb67cf13903115fc3245fc79fca125
Heri Susanto <susantoh41@gmail.com>
2020-03-31 11:08:11 +0700

    delete unusused files

--- Web For C/forc/dina_metakeyword.php
+++
@@ -1,7 +0,0 @@
-<?php
-    $query = "SELECT meta_keyword FROM identitas";
-    $hasil = mysqli_query($konek, $query);
-    $data  = mysqli_fetch_array($hasil);
-    
-    echo "$data[meta_keyword]";
-?>
--- Web For C/forc/dina_title.php
+++
@@ -1,6 +0,0 @@
-<?php
-    $query = "SELECT judul_website FROM identitas";
-    $hasil = mysqli_query($konek, $query);
-    $data  = mysqli_fetch_array($hasil);
-	  echo "$data[judul_website]";
-?>
